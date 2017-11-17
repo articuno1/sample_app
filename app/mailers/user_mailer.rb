2c6e2d625c58cab78fa5eafaa00d7874cc11a10e
leducduy <duyanh.3895@gmail.com>
2017-11-17 09:34:06 +0700

    Finished-Chapter12: Password-Reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,9 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: t("user_mailer.subject")
   end
+
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("user_mailer.reset_pass")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "password_resets/new"
+  get "password_resets/edit"
+
   root "static_pages#home"
 
   get "users/new"
@@ -12,5 +15,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users
-  resources :account_activations, only: [:edit]
+  resources :account_activations, only: :edit
+  resources :password_resets, except: %i(index show destroy)
 end
